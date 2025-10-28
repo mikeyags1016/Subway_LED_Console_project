bd801a6a1fb83da65423ac83fe100a0e6ce8ea47
Michael <magsam2@nd.edu>
2025-10-28 13:59:35 -0400

    EPS32 data structure update

--- ESP32/LED_power_router.ino
+++ ESP32/LED_power_router.ino
@@ -10,9 +10,6 @@
 #include <Arduino.h>
 #include "WS2812BStrip.h"
 
-#include <Arduino.h>
-#include "WS2812BStrip.h"
-
 #define LED_PIN   5
 #define LED_COUNT 60
 
@@ -22,60 +19,84 @@ LED leds[LED_COUNT];
 LEDStrip strip(leds, LED_COUNT);
 
 class LEDList {
-  private:
+private:
     LED* head;
+    LED* tail;
     unsigned int size;
 
-  public:
-    LEDList(LED* leds, unsigned int count) {
-      head = &leds[0];
-      size = count;
+public:
+    LEDList() : head(nullptr), tail(nullptr), size(0) {}
 
-      for (unsigned int i = 0; i < count-1; i++) {
-        if (i == 0) {
-          leds[i].prev = nullptr;
+    LEDList(LED* leds, unsigned int count) {
+        initialize(leds, count);
+    }
+
+    void initialize(LED* leds, unsigned int count) {
+        head = &leds[0];
+        size = count;
+
+        for (unsigned int i = 0; i < count; i++) {
+            leds[i].prev = (i == 0) ? nullptr : &leds[i - 1];
+            leds[i].next = (i == count - 1) ? nullptr : &leds[i + 1];
         }
-        else {
-          leds[i].prev = &leds[i-1];
-        }
-        if (i == count - 1) {
-          leds[i].next = nullptr;          
-        } else {
-          leds[i].next = &leds[i + 1];    
-        }
-      }
+
+        tail = &leds[count - 1];
     }
 
     LED* getHead() { return head; }
     unsigned int getSize() { return size; }
 
     void setAll(uint8_t r, uint8_t g, uint8_t b) {
-      LED* current = head;
-      while (current) {
-        current->red   = r;
-        current->green = g;
-        current->blue  = b;
-        current = current->next;
-      }
+        LED* current = head;
+        while (current) {
+            current->red = r;
+            current->green = g;
+            current->blue = b;
+            current = current->next;
+        }
     }
 
     void setOne(int index, uint8_t r, uint8_t g, uint8_t b) {
-      if (index < 0 || index >= size) return;
-      LED* current = head;
-      for (int i = 0; i < index; i++) {
-        current = current->next;
-      }
-      current->red = r;
-      current->green = g;
-      current->blue = b;
+        if (index < 0 || index >= size) return;
+        LED* current = head;
+        for (int i = 0; i < index; i++)
+            current = current->next;
+
+        current->red = r;
+        current->green = g;
+        current->blue = b;
     }
 };
 
-// TODO: ROUTE POWER TO CORRECT DATALINE ON LED BOARD, FIGURE OUT THIS WITH VINCE AND ANDREW
-void dataline_route(LEDList route) {
+class LEDMap {
+public:
+    static const int NUM_ROUTES = 9;
+    static const int ROUTE_LENGTH = 60;
 
+    LED leds[NUM_ROUTES * ROUTE_LENGTH];  
+    LEDList routes[NUM_ROUTES];
+
+    LEDMap();
+};
+
+LEDMap::LEDMap() {
+    for (int i = 0; i < NUM_ROUTES; i++) {
+        LED* start = &leds[i * ROUTE_LENGTH];
+        routes[i].initialize(start, ROUTE_LENGTH);
+    }
 }
 
+// TODO: ROUTE POWER TO CORRECT DATALINE ON LED BOARD, FIGURE OUT THIS WITH VINCE AND ANDREW
+void dataline_route(LEDList route, int starting_index) {
+  // Path given from API: A1 -> A2 -> (branch) B1 -> B2 -> B3
+  // Ask vince to send the line for the starting node as well
+  LEDMap map;
+
+  // Check if you need to go up or down the line
+
+  // Figure out how the output is going to look like 
+  
+}
 
 LEDList ledList(leds, LED_COUNT);
 
