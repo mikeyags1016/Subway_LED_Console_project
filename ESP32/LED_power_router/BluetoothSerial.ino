7d2ac82d33ea86e196c4fd75a6d01db2c2b3e3a9
adomsall <156740488+adomsall@users.noreply.github.com>
2025-11-12 23:30:54 -0500

    Update BluetoothSerial.ino

--- ESP32/LED_power_router/BluetoothSerial.ino
+++ ESP32/LED_power_router/BluetoothSerial.ino
@@ -8,6 +8,13 @@ void setup() {
   Serial.println("Bluetooth started! Pair with ESP32_BT");
 }
 
+// Get and print the Bluetooth MAC address
+  const uint8_t* mac = esp_bt_dev_get_address();
+  Serial.printf("Bluetooth device name: %s\n", "ESP32_BT");
+  Serial.printf("Bluetooth MAC address: %02X:%02X:%02X:%02X:%02X:%02X\n",
+                mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+}
+
 void loop() {
   if (Serial.available()) {
     SerialBT.write(Serial.read()); // forward from USB to Bluetooth
